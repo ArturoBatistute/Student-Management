33d6dde664f1b601a248d2a925e885ebf9fd3a36
ArturoBatistute <arturo.batistute@gmail.com>
2021-11-28 21:33:36 -0300

    Add student repository with JPA methods

--- src/main/java/com/master/studentManagement/student/StudentService.java
+++ src/main/java/com/master/studentManagement/student/StudentService.java
@@ -1,23 +1,18 @@
 package com.master.studentManagement.student;
 
-import java.time.LocalDate;
-import java.time.Month;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StudentService {
 	
+	@Autowired
+	private StudentRepository studentRepository;
+	
 	public List<Student> getStudents(){
-		return List.of(
-				new Student(
-				1L,
-				"Astra",
-				"Astra@gmail.com",
-				LocalDate.of(2000, Month.JANUARY, 12)
-				,21
-				)
-			);
+		
+		return studentRepository.findAll();
 	}
 }
