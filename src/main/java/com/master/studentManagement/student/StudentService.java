52c3d5a642e4bea4d26d0e769ccfe9a8ab133b86
ArturoBatistute <arturo.batistute@gmail.com>
2021-11-28 22:29:58 -0300

    Add save student API method and configuration to populate the database.

--- src/main/java/com/master/studentManagement/student/StudentService.java
+++ src/main/java/com/master/studentManagement/student/StudentService.java
@@ -1,6 +1,8 @@
 package com.master.studentManagement.student;
 
+import java.awt.IllegalComponentStateException;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,5 +16,19 @@ public class StudentService {
 	public List<Student> getStudents(){
 		
 		return studentRepository.findAll();
+	}
+	
+	public Student saveStudent(Student newStudent){
+		
+		final Optional<Student> existentStudentByEmail = studentRepository.findStudentByEmail(newStudent.getEmail());
+		
+		if(existentStudentByEmail.isPresent()) {
+			
+			throw new IllegalComponentStateException("Email already in usage.");
+		} 
+		else
+		{
+			return studentRepository.save(newStudent);
+		}
 	}
 }
